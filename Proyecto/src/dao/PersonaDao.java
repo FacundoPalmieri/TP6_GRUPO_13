dbd7bfc65bb699d13bf4e999ac836b513a857510
Robielys <116286724+Robielys@users.noreply.github.com>
2024-05-21 12:51:09 -0300

    se crea paquete dao y la clase personaDao

--- Proyecto/src/dao/PersonaDao.java
+++ Proyecto/src/dao/PersonaDao.java
@@ -1,5 +1,45 @@
 package dao;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import entidad.Persona;
+
 public class PersonaDao {
 
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user ="root";
+	private String pass = "root";
+	private String dbName = "dbpersonas";
+
+	public PersonaDao()
+	{
+	
+	}
+	
+	public int AgregarPersona (Persona persona)
+	{
+		String query = "insert into Personas (Dni,Nombre,Apellido)values ('"+persona.getDni() + "', '" + persona.getNombre() + "', '" + persona.getApellido() + "')";
+		Connection cn = null;
+		int filas=0;
+		
+		try {
+            cn = DriverManager.getConnection(host + dbName, user, pass);
+            Statement st = cn.createStatement();
+            filas = st.executeUpdate(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cn != null) {
+                try {
+                    cn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return filas;
+    }
 }
